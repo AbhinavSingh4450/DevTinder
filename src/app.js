99b11124b28b9578f20bc7109cf433476374ed72
Abhinav Singh <sabhinavpratap2@gmail.com>
2024-12-30 12:50:50 +0530

    practicing routes

--- src/app.js
+++ src/app.js
@@ -1,22 +1,56 @@
 const express = require('express');
 const app = express();
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from the test");
+app.use("/user/:userId/:name",(req,res)=>{
+    console.log(req.params);
+    res.send(req.params);
+})
 
+app.get("/ab+c", (req,res)=>{
+    res.send("hahahhah");
+})
+
+app.get("/de?f", (req,res)=>{
+    res.send("hehehe");
+})
+
+app.get("/test",(req,res)=>{
+ 
+    res.send({
+        "name": "Abhinav Singh",
+        "city":"Agra"
+    })
 });
 
-app.use("/namaste", (req, res)=>{
-    res.send("Namaste from the server");
-
+app.post("/test",(req,res)=>{
+    res.send("This is post from the test");
 });
 
-
-app.use((req, res)=>{
-    res.send("Hello from the main deshboard of server");
-
+app.delete("/test",(req,res)=>{
+    res.send("This is delete from the test");
 });
+
+// app.use("/test", (req, res)=>{
+//     res.send("Hello from the test");
+
+// });
+
+// app.use("/namaste", (req, res)=>{
+//     res.send("Namaste from the server");
+
+// });
+
+// app.use("/", (req, res)=>{
+//     res.send("The most basic call!!");
+// });
+
+
+// app.use((req, res)=>{
+//     res.send("Hello from the main deshboard of server");
+
+// });
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
 });
+
