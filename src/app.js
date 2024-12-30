b431d67985a2c3c57cf8a7786e3a5af5bbbb288e
Abhinav Singh <sabhinavpratap2@gmail.com>
2024-12-30 22:08:19 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,54 +1,29 @@
 const express = require('express');
 const app = express();
 
-app.use("/user/:userId/:name",(req,res)=>{
-    console.log(req.params);
-    res.send(req.params);
-})
+app.use(
+    "/user",
+     (req,res,next)=>{
+    console.log("I am 1 Route Handler");
+    // res.send("Response 1");
+    next();
+    },
+  [  (req,res,next)=>{
+        console.log("I am 2 Route Handler");
+        // res.send("Response 2");
+        next();
+    } ,
+    (req,res, next)=>{
+        console.log("I am 3 Route Handler");
+        // res.send("Response 3");
+        next();
+    } ],
+    (req,res,next)=>{
+        console.log("I am 4 Route Handler");
+        res.send("Response 4");
 
-app.get("/ab+c", (req,res)=>{
-    res.send("hahahhah");
-})
-
-app.get("/de?f", (req,res)=>{
-    res.send("hehehe");
-})
-
-app.get("/test",(req,res)=>{
- 
-    res.send({
-        "name": "Abhinav Singh",
-        "city":"Agra"
-    })
-});
-
-app.post("/test",(req,res)=>{
-    res.send("This is post from the test");
-});
-
-app.delete("/test",(req,res)=>{
-    res.send("This is delete from the test");
-});
-
-// app.use("/test", (req, res)=>{
-//     res.send("Hello from the test");
-
-// });
-
-// app.use("/namaste", (req, res)=>{
-//     res.send("Namaste from the server");
-
-// });
-
-// app.use("/", (req, res)=>{
-//     res.send("The most basic call!!");
-// });
-
-
-// app.use((req, res)=>{
-//     res.send("Hello from the main deshboard of server");
-
-// });
+    }  
+)
 
 app.listen(3000, ()=>{
     console.log("Server is successfully listening on port 3000...");
