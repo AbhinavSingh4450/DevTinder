c82ef3e4b987153d0e57e7c0f1bd24e0208a5a6e
Abhinav Singh <sabhinavpratap2@gmail.com>
2025-01-09 16:21:41 +0530

    using express.Router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
@@ -9,135 +9,97 @@ const JWT = require("jsonwebtoken");
 const {userAuth}=require("./middlewares/auth");
 const user = require('./models/user');
 
+const authRouter=require('./routes/auth');
+const profileRouter=require("./routes/profile");
+const requestRouter = require("./routes/request");
+
+
 
 app.use(cookieParser());
 app.use(express.json());
 
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
+
+
 // Creating Signup Api
-app.post("/signup", async (req,res)=>{
+// app.post("/signup", async (req,res)=>{
 
    
-    const {firstName, lastName, emailId, password}= req.body;
+//     const {firstName, lastName, emailId, password}= req.body;
 
-     // Encrypt the password 
-     const passwordHash = await bcrypt.hash(password,10);
+//      // Encrypt the password 
+//      const passwordHash = await bcrypt.hash(password,10);
 
-     // Create instance of the User model
-    const user= new User({
-        firstName,
-        lastName,
-        emailId,
-        password: passwordHash
-    });
-    try{
+//      // Create instance of the User model
+//     const user= new User({
+//         firstName,
+//         lastName,
+//         emailId,
+//         password: passwordHash
+//     });
+//     try{
 
-    // Validate the data
-    validateSignUpData(req);
+//     // Validate the data
+//     validateSignUpData(req);
 
 
-    await user.save();
-    res.send("User Added Sucessfully");
-      }
+//     await user.save();
+//     res.send("User Added Sucessfully");
+//       }
       
-    catch(err){
-        res.status(400).send("ERROR:"+err.message);
-    }
-})
+//     catch(err){
+//         res.status(400).send("ERROR:"+err.message);
+//     }
+// })
 
-app.get("/profile", userAuth, async(req,res)=>{
-    try{
-       const user =req.user;
-       res.send(user);
-    }
-    catch(err){
-        res.status(400).send("ERROR:"+err.message);
-    }
-})
+// app.get("/profile", userAuth, async(req,res)=>{
+//     try{
+//        const user =req.user;
+//        res.send(user);
+//     }
+//     catch(err){
+//         res.status(400).send("ERROR:"+err.message);
+//     }
+// })
 
 // Creating Login API
-app.post("/login",async (req,res)=>{
-    try{
-        const {password, emailId, userId}=req.body;
+// app.post("/login",async (req,res)=>{
+//     try{
+//         const {password, emailId, userId}=req.body;
 
-        const user= await User.findOne({emailId:emailId});
-        if(!user){
-            throw new Error("Invalid credentials");
-        }
-        const isValidPassword= await user.validatePassword(password);
-        if (!isValidPassword){
-            throw new Error("Invalid credentials");
-        }
+//         const user= await User.findOne({emailId:emailId});
+//         if(!user){
+//             throw new Error("Invalid credentials");
+//         }
+//         const isValidPassword= await user.validatePassword(password);
+//         if (!isValidPassword){
+//             throw new Error("Invalid credentials");
+//         }
 
-        // Create a JWT Token
-        const token = await user.getJWT();
+//         // Create a JWT Token
+//         const token = await user.getJWT();
 
 
 
-        // Add token to cookie and send response back to user
-        res.cookie("token",token, {
-            expires: new Date(Date.now()+ 8*3600000)
-        });
+//         // Add token to cookie and send response back to user
+//         res.cookie("token",token, {
+//             expires: new Date(Date.now()+ 8*3600000)
+//         });
         
 
-        res.send("Login Successful")
-    }
-    catch(err){
-        res.status(400).send("ERROR:"+err.message);
-    }
-    
-})
-
-app.post("/sendConnectionRequest", userAuth, async(req,res)=>{
-    const user = req.user;
-    res.send(user.firstName+" Sent you a connection request");
-})
-
-// Getting all the users to create a feed
-// app.get("/feed",async (req,res)=>{
-
-//     try{
-//        const users= await User.find({});
-//        if(users.length==0){
-//         res.status(404).send("No user found");
-//        }
-//        else{
-//         console.log("user add hogya hai");
-//         res.send(users);
-//        }
-    
+//         res.send("Login Successful")
 //     }
 //     catch(err){
-//        res.status(400).send("Something went Wrong");
-//     } 
+//         res.status(400).send("ERROR:"+err.message);
+//     }
+    
 // })
 
-// app.delete("/user", async(req,res)=>{
-//     const id= req.body.id;
-//     await User.findByIdAndDelete({_id: id})
-//     res.send("User deleted successfully");
-// })
-
-// app.patch("/user", async(req,res)=>{
-//     const userId=req.body.userId;
-//     const data = req.body;
-//     try{
-//        const ALLOWED_UPDATES=["userId","firstName","lastName","skills","about","gender"] ;
-//        const isUpdateAllowed= Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k));
-//        if(!isUpdateAllowed){
-//         throw new Error("Update not allowed");
-//        }
-
-//        const user = await User.findByIdAndUpdate({_id : userId},data,{
-//         runValidators:true
-//        } );
-//         console.log(user);
-//         res.send("User updated Successfully");
-//     }
-//     catch(err){
-//         res.status(400).send("Something went Wrong");
-//     }
-   
-
+// app.post("/sendConnectionRequest", userAuth, async(req,res)=>{
+//     const user = req.user;
+//     res.send(user.firstName+" Sent you a connection request");
 // })
 
 connectDB()
---
+++ src/routes/auth.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const authRouter = express.Router();
+
+const User = require("../models/user");
+const {validateSignUpData} = require("../utils/validation");
+const bcrypt = require('bcrypt');
+
+
+// Creating Signup API
+authRouter.post("/signup", async (req,res)=>{
+
+   
+    const {firstName, lastName, emailId, password}= req.body;
+
+     // Encrypt the password 
+     const passwordHash = await bcrypt.hash(password,10);
+
+     // Create instance of the User model
+    const user= new User({
+        firstName,
+        lastName,
+        emailId,
+        password: passwordHash
+    });
+    try{
+
+    // Validate the data
+    validateSignUpData(req);
+
+
+    await user.save();
+    res.send("User Added Sucessfully");
+      }
+      
+    catch(err){
+        res.status(400).send("ERROR:"+err.message);
+    }
+})
+
+// Creating Login API
+authRouter.post("/login",async (req,res)=>{
+    try{
+        const {password, emailId, userId}=req.body;
+
+        const user= await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+        const isValidPassword= await user.validatePassword(password);
+        if (!isValidPassword){
+            throw new Error("Invalid credentials");
+        }
+
+        // Create a JWT Token
+        const token = await user.getJWT();
+
+
+
+        // Add token to cookie and send response back to user
+        res.cookie("token",token, {
+            expires: new Date(Date.now()+ 8*3600000)
+        });
+        
+
+        res.send("Login Successful")
+    }
+    catch(err){
+        res.status(400).send("ERROR:"+err.message);
+    }
+    
+})
+
+module.exports=authRouter;
